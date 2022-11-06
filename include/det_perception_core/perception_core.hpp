efb40e497be72f1fdb8ffc3b252356e6d23d7600
Jiasen Zheng <jiasenzheng2020@u.northwestern.edu>
2022-11-05 19:38:45 -0500

    plane segmentation

--- include/det_perception_core/perception_core.hpp
+++ include/det_perception_core/perception_core.hpp
@@ -9,12 +9,17 @@
 #include <ros/ros.h>
 #include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
 #include <opencv2/opencv.hpp>
 #include <opencv2/calib3d.hpp>
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/Image.h>
 #include <cv_bridge/cv_bridge.h>
-#include <pcl_conversions/pcl_conversions.h>
 
 class PerceptionCore
 {
@@ -23,6 +28,13 @@ public:
     void run();
     void pointcloudCallback(const sensor_msgs::PointCloud2ConstPtr& msg);
     void imageCallback(const sensor_msgs::ImageConstPtr& msg);
+    template <typename T>
+    void planeSegmentation(const typename pcl::PointCloud<T>::Ptr cloud, int max_iterations, 
+    double distance_threshold, pcl::PointIndices::Ptr inliers, pcl::ModelCoefficients::Ptr coefficients);
+    template <typename T>
+    pcl::PointCloud<pcl::PointXYZRGB> colorizeSegmentation(const typename pcl::PointCloud<T>::Ptr cloud, 
+    const pcl::PointIndices::Ptr inliers);
+
 private:
     ros::NodeHandle m_nh;
     std::string m_lidar_topic;
--- src/perception_core.cpp
+++ src/perception_core.cpp
@@ -16,17 +16,60 @@ PerceptionCore::PerceptionCore(ros::NodeHandle nh): m_nh(nh)
 
 void PerceptionCore::run()
 {
+    
 }
 
 void PerceptionCore::pointcloudCallback(const sensor_msgs::PointCloud2ConstPtr& msg)
 {
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::fromROSMsg(*msg, *cloud);
+    pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
+    pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
+    planeSegmentation<pcl::PointXYZ>(cloud, 100, 0.01, inliers, coefficients);
+    pcl::PointCloud<pcl::PointXYZRGB> cloud_colored = colorizeSegmentation<pcl::PointXYZ>(cloud, inliers);
     sensor_msgs::PointCloud2 processed_msg;
-    pcl::toROSMsg(*cloud, processed_msg);
+    pcl::toROSMsg(cloud_colored, processed_msg);
     processed_msg.header.frame_id = msg->header.frame_id;
     processed_msg.header.stamp = msg->header.stamp;
     m_pointcloud_pub.publish(processed_msg);
+}
+
+template <typename T>
+void PerceptionCore::planeSegmentation(const typename pcl::PointCloud<T>::Ptr cloud, int max_iterations, 
+double distance_threshold, pcl::PointIndices::Ptr inliers, pcl::ModelCoefficients::Ptr coefficients) {
+    pcl::SACSegmentation<T> seg;
+    seg.setOptimizeCoefficients(true);
+    seg.setModelType(pcl::SACMODEL_PLANE);
+    seg.setMethodType(pcl::SAC_RANSAC);
+    seg.setMaxIterations(max_iterations);
+    seg.setDistanceThreshold(distance_threshold);
+    seg.setInputCloud(cloud);
+    seg.segment(*inliers, *coefficients);
+}
+
+template <typename T>
+pcl::PointCloud<pcl::PointXYZRGB> PerceptionCore::colorizeSegmentation(const typename pcl::PointCloud<T>::Ptr cloud, 
+const pcl::PointIndices::Ptr inliers) {
+    pcl::PointCloud<pcl::PointXYZRGB> colored_cloud;
+    colored_cloud.width = cloud->width;
+    colored_cloud.height = cloud->height;
+    colored_cloud.is_dense = cloud->is_dense;
+    colored_cloud.points.resize(cloud->points.size());
+    for (size_t i = 0; i < cloud->points.size(); i++) {
+        colored_cloud.points[i].x = cloud->points[i].x;
+        colored_cloud.points[i].y = cloud->points[i].y;
+        colored_cloud.points[i].z = cloud->points[i].z;
+        if (std::find(inliers->indices.begin(), inliers->indices.end(), i) != inliers->indices.end()) {
+            colored_cloud.points[i].r = 255;
+            colored_cloud.points[i].g = 0;
+            colored_cloud.points[i].b = 0;
+        } else {
+            colored_cloud.points[i].r = 0;
+            colored_cloud.points[i].g = 255;
+            colored_cloud.points[i].b = 0;
+        }
+    }
+    return colored_cloud;
 }
 
 int main(int argc, char** argv)
