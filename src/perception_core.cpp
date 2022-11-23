036232a16f3fcf8c2827d7ee336c286812e0846b
Jiasen Zheng <jiasenzheng2020@u.northwestern.edu>
2022-11-23 15:18:57 -0600

    change to mesh

--- src/perception_core.cpp
+++ src/perception_core.cpp
@@ -229,7 +229,8 @@ void PerceptionCore::pointcloudCallback(const sensor_msgs::PointCloud2ConstPtr& 
         marker.header.stamp = msg->header.stamp;
         marker.ns = "cluster_" + std::to_string(i);
         marker.id = 0;
-        marker.type = visualization_msgs::Marker::CUBE;
+        marker.type = visualization_msgs::Marker::MESH_RESOURCE;
+        marker.mesh_resource = "https://raw.githubusercontent.com/JiasenZheng/det_perception_core/main/meshes/textured.dae";
         marker.action = visualization_msgs::Marker::ADD;
         marker.pose.position.x = position[0];
         marker.pose.position.y = position[1];
@@ -238,10 +239,10 @@ void PerceptionCore::pointcloudCallback(const sensor_msgs::PointCloud2ConstPtr& 
         marker.pose.orientation.y = orientation.y();
         marker.pose.orientation.z = orientation.z();
         marker.pose.orientation.w = orientation.w();
-        marker.scale.x = dimensions[0];
-        marker.scale.y = dimensions[1];
-        marker.scale.z = dimensions[2];
-        marker.color.a = 0.5;
+        marker.scale.x = 1.0;
+        marker.scale.y = 1.0;
+        marker.scale.z = 1.0;
+        marker.color.a = 0.8;
         marker.color.r = m_colors[i % 6][0] / 255.0;
         marker.color.g = m_colors[i % 6][1] / 255.0;
         marker.color.b = m_colors[i % 6][2] / 255.0;
